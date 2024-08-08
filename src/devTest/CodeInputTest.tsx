4be8d220dbfed28d002445461ff2e87880791324
ldulivo <ldulivo@gmail.com>
2024-08-08 20:41:02 +0200

    demonstration example

--- src/devTest/CodeInputTest.tsx
+++ src/devTest/CodeInputTest.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { CodeInput } from "../Elements/CodeInput";
 
 const CodeInputTest: React.FC = () => {
-  const myCode = "<div>Esto es un título</div>";
+  const myCode = "<h1>This is a title</h1>\n<div>\n <p>Lorem ipsum dolor sit.</p>//Paragraph\n</div>\n//End of code";
   return (
     <div style={{width: 400, height: 300, maxHeight: 300}}>
       <CodeInput code={ myCode }/>
