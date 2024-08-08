546efdac5694750d3303bba2035d9b9a1dc593c2
ldulivo <ldulivo@gmail.com>
2024-08-08 20:21:07 +0200

    replace div with other elements

--- src/core/Container/Container.tsx
+++ src/core/Container/Container.tsx
@@ -20,30 +20,25 @@ const Container: React.FC<ContainerProps> = ({
 
   return (
     <div
+      className={`Container ${isFocused ? "Container--isFocused" : ""} ${darkMode ? "darkMode" : ""}`}
+      style={{ ...otherStyles, border, borderRadius, borderColor, borderStyle}}
       onFocus={() => setIsFocused(true)}
       onBlur={() => setIsFocused(false)}
-      className={`Container--setup ${isFocused ? "isFocused" : ""} ${darkMode ? "darkMode" : ""}`}
     >
-      <div className="Container" style={otherStyles}>
-        <div className="Container--LeftSide" style={borderProps}></div>
-        <div className="Container--Content">
-          <div className="Container--TopElement">
-            <div className="Container--TopElement-main">
-              {label
-                ? <label
-                    onClick={handleClickLabel}
-                    className={`Container--Label ${hasContent ? "Container--LabelActive" : ""}`}
-                  >{label}</label>
-                : null
-              }
-              <div className={`Container--TopSide ${hasContent ? "Container--TopSideActive" : ""}`} style={borderProps}></div>
-            </div>
-          </div>
-          <div className="Container--Children">{children}</div>
-          <div className="Container--BottomSide" style={borderProps}></div>
-        </div>
-        <div className="Container--RightSide" style={borderProps}></div>
-      </div>
+      <main>
+        <section>
+          <div style={borderProps}></div>
+            {label
+              ? <label
+                  onClick={handleClickLabel}
+                  className={`${hasContent ? "Container--LabelActive" : ""}`}
+                >{label}</label>
+              : null
+            }
+            <div style={borderProps}></div>
+        </section>
+      </main>
+      <div className="Container--Children">{children}</div>
     </div>
   );
 };
